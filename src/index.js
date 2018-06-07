8aeec70fdf5265a60acdbfe5fffeaf70baf31328
brainsoap <vjaszai@brainsum.com>
2018-06-07 09:06:20 +0200

    Modal slider functionality added.

--- src/index.js
+++ src/index.js
@@ -10,22 +10,26 @@ document.CookieConsent.config = {
   categories: {
     necessary: {
       name: 'Strictly Necessary Cookies',
-      active: true,
+      needed: true,
+      wanted: true,
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper.'
     },
-    functional: {
+    performance: {
       name: 'Performance Cookies',
-      active: false,
+      needed: false,
+      wanted: true,
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper.'
     },
     social: {
       name: 'Social media',
-      active: false,
+      needed: false,
+      wanted: true,
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper.'
     },
     targeting: {
       name: 'Targeting Cookies',
-      active: false,
+      needed: false,
+      wanted: true,
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper.'
     },
   },
@@ -52,10 +56,12 @@ document.CookieConsent.buffer = {
   insertBefore: []
 }
 
-;(function () {
+;(function (Cookie) {
   
   var consent = false;
-  var dasad;
+
+
+  cookieToConfig();
 
   // Checking for the value in cookie named "consent"
   if (document.cookie.indexOf('consent=1') < 0 ) {
@@ -74,8 +80,7 @@ document.CookieConsent.buffer = {
 
     if ( ! consent) {
       if(arguments[0].tagName === 'SCRIPT') {
-        console.log(arguments[0])
-        document.CookieConsent.buffer.appendChild.push({'this': this, arguments: arguments});
+        Cookie.buffer.appendChild.push({'this': this, arguments: arguments});
         return undefined;
       }
     }
@@ -89,11 +94,10 @@ document.CookieConsent.buffer = {
 
     if ( ! consent) {
       if(arguments[0].tagName === 'SCRIPT') {
-        for (let key in document.CookieConsent.config.services) {
-          console.log(key)
-          if(document.CookieConsent.config.services[key].type === 'script-tag') {
-            if(arguments[0].outerHTML.indexOf(document.CookieConsent.config.services[key].search) >= 0) {
-              document.CookieConsent.buffer.insertBefore.push({'this': this, arguments: arguments});
+        for (let key in Cookie.config.services) {
+          if(Cookie.config.services[key].type === 'script-tag') {
+            if(arguments[0].outerHTML.indexOf(Cookie.config.services[key].search) >= 0) {
+              Cookie.buffer.insertBefore.push({'this': this, arguments: arguments});
               return undefined;
             }
           }
@@ -105,7 +109,6 @@ document.CookieConsent.buffer = {
   }
 
 
-
   buildInterface(function(){
 
     var cookieModal = document.getElementById('cookie-modal');
@@ -114,16 +117,17 @@ document.CookieConsent.buffer = {
     document.getElementById('consent-give').addEventListener('click', function () {
 
       consent = true;
-      console.log('Cookie consent given!')
-
-      document.cookie = 'consent=1; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/;';
       
-      document.CookieConsent.buffer.appendChild.forEach(function(action, index){
+      Cookie.buffer.appendChild.forEach(function(action, index){
         appendChild.apply(action.this, action.arguments);
       });
 
-      document.CookieConsent.buffer.insertBefore.forEach(function(action){
+      Cookie.buffer.insertBefore.forEach(function(action){
         insertBefore.apply(action.this, action.arguments);
+      });
+
+      buildCookie((cookie) => {
+        setCookie(cookie);
       });
 
     });
@@ -133,6 +137,7 @@ document.CookieConsent.buffer = {
       cookieModal.classList.add('visible');
     });
 
+    // If you click trough the tabs on Cookie settings
     cookieModal.querySelector('.left').addEventListener('click', function(event){
       if (event.target.classList.contains('tab')) {
         if (event.target.dataset.tab) {
@@ -153,23 +158,21 @@ document.CookieConsent.buffer = {
       }
     });
 
+    // If you click submit on cookie settings
+    document.getElementById('cookie-modal-submit').addEventListener('click', function(event){
+      cookieModal.querySelectorAll('.switch input').forEach(function(elem){
+        Cookie.config.categories[elem.dataset.category].wanted = elem.checked;
+      });
+
+      buildCookie((cookie) => {
+        setCookie(cookie, () => {
+          cookieModal.classList.remove('visible');
+        });
+      });
+
+    });
   });
-
-
-
-  // document.addEventListener('readystatechange', function (event) {
-  //   if (event.target.readyState === "complete") {
-        
-
-  //     document.getElementById('consent-revoke').addEventListener('click', function () {
-  //       console.log('revoke');
-  //       document.cookie = "consent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
-  //     });
-  //   }
-  // });
-
-})();
+})(document.CookieConsent);
 
 
 
@@ -181,7 +184,37 @@ function ready(fn) {
   }
 }
 
+function buildCookie(callback) {
+  let cookie = {};
+  
+  for(let key in document.CookieConsent.config.categories) {
+    cookie[key] = document.CookieConsent.config.categories[key].wanted;
+  }
 
+  if (callback) callback(cookie);
+  return cookie;
+}
+
+function setCookie(cookie, callback) {
+  document.cookie = `cconsent=${JSON.stringify(cookie)}; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/;`;
+  if (callback) callback();
+}
+
+function removeCookie(cookie) {
+  document.cookie = `cconsent=; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/;`;
+}
+
+function cookieToConfig() {
+  (document.cookie.split(';').filter((item) => {
+    if (item.includes('cconsent')) {
+      var cookieData = JSON.parse(item.split('=')[1]);
+    }
+    for (let key in cookieData) {
+      document.CookieConsent.config.categories[key].wanted = cookieData[key];
+    }
+    console.log(document.CookieConsent.config.categories);
+  }).length)
+}
 
 function buildInterface(callback) {
 
@@ -207,6 +240,16 @@ function buildInterface(callback) {
     '#cookie-modal .tab.active {background-color:#FFF;}',
     '#cookie-modal [class^=tab-content] {display:none; padding:10px 20px}',
     '#cookie-modal [class^=tab-content].visible {display:block}',
+    '#cookie-modal [class^=tab-content] .head {position: relative}',
+    '#cookie-modal [class^=tab-content] .switch {position: absolute; top:0; right:0; display: inline-block; width: 40px; height: 20px;}',
+    '#cookie-modal [class^=tab-content] .switch input {display:none;}',
+    '#cookie-modal [class^=tab-content] .switch .slider  {position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; -webkit-transition: .4s; transition: .4s;}',
+    '#cookie-modal [class^=tab-content] .switch .slider:before  {position: absolute; content: ""; height: 12px; width: 12px; left: 4px; bottom: 4px; background-color: white; -webkit-transition: .4s; transition: .4s;}',
+    '#cookie-modal [class^=tab-content] .switch input:checked + .slider  {background-color: #2196F3;}',
+    '#cookie-modal [class^=tab-content] .switch input:focus + .slider  {box-shadow: 0 0 1px #2196F3;}',
+    '#cookie-modal [class^=tab-content] .switch input:checked + .slider:before  {-webkit-transform: translateX(20px); -ms-transform: translateX(20px); transform: translateX(20px);}',
+    '#cookie-modal .footer {border-top:1px solid #D8D8D8; padding:10px; text-align:right;}',
+    '#cookie-modal .footer button {border:none; background-color:#63B3E3; color:#FFF; padding:10px; cursor:pointer}',
     );
 
     var bar =
@@ -234,7 +277,13 @@ function buildInterface(callback) {
 
       contentItems.push(el('div.tab-content-first.visible', [el('h3', 'Your Privacy'), el('p', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper. Nam eros sem, varius et vehicula sagittis, vulputate sed augue. Quisque id sem bibendum, convallis odio ac, egestas tellus. Duis rhoncus rutrum metus et maximus.')])); 
       for (let key in document.CookieConsent.config.categories) {
-        contentItems.push(el('div.tab-content-' + key, [el('h3', document.CookieConsent.config.categories[key].name), el('p', document.CookieConsent.config.categories[key].text)])); 
+        contentItems.push(el('div.tab-content-' + key,
+                            el('div.head',
+                              el('h3', document.CookieConsent.config.categories[key].name),
+                              ( ! document.CookieConsent.config.categories[key].needed) && el('label.switch',
+                                  el('input', {type:'checkbox', 'data-category': key, checked:(document.CookieConsent.config.categories[key].wanted) ? true : false}), el('span.slider'))),
+                            el('div.body',
+                              [el('p', document.CookieConsent.config.categories[key].text)]))); 
       }
       contentItems.push(el('div.tab-content-last', [el('h3', 'More Information'), el('p', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu commodo est, nec gravida odio. Suspendisse scelerisque a ex nec semper. Nam eros sem, varius et vehicula sagittis, vulputate sed augue. Quisque id sem bibendum, convallis odio ac, egestas tellus. Duis rhoncus rutrum metus et maximus.')])); 
       
@@ -244,10 +293,12 @@ function buildInterface(callback) {
     var modal =
     el('div#cookie-modal',
       el('div.content',
-        [el('div.heading',
+        el('div.heading',
           el('h2', 'Cookie settings')),
         el('div.left', modalTabList),
-        el('div.right', modalTabContentList)]));
+        el('div.right', modalTabContentList),
+        el('div.footer',
+          el('button#cookie-modal-submit', 'Submit', ))));
 
     mount(document.body, bar);
     mount(document.body, modal);
